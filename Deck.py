65567d89f51e42ad4ad70afc25649ad0e0d3624f
Your Name <you@example.com>
2025-02-09 19:07:26 +0100

    Add CardLoader and Deck classes for card management functionality

---
+++ Deck.py
@@ -0,0 +1,27 @@
+from typing import List
+from Card import Card
+import random
+
+class Deck:
+	def __init__(self, deck: List[str]):
+		self.cards: List[str] = deck # List of cards in the by ID
+		self.hand: List[str] = []
+		self.graveyard: List[str] = []
+		self.deck: List[str] = random.shuffle(deck)
+	
+	def take_cards(self, n: int) -> List[str]:
+		if n > len(self.deck):
+			n = len(self.deck)
+		cards = self.deck[:n]
+		self.hand.extend(cards)
+		self.deck = self.deck[n:]
+		return cards
+	
+	def play_card(self, card: str):
+		self.hand.remove(card)
+
+	def discard_card(self, card: str):
+		self.graveyard.append(card)
+	
+
+		
